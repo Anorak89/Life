3a5680e8a37aba1e30eb6c83a56fd4bfee483317
Anorak89 <yaskor27@bergen.org>
2024-05-16 13:04:07 -0400

    Removed

--- MyWorld.java
+++ MyWorld.java
@@ -8,8 +8,6 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-    //GYATT MR ZHANG GOT THAT DUMPY
-    //AYY YOU AINT WRONG THO
     /**
      * Constructor for objects of class MyWorld.
      * 
